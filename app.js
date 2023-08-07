08cab72b9d6f92b17f177980d3326db9528e4404
Muhammad-Hassan1 <MuhammadHassan88195@gmail.com>
2023-08-07 22:08:58 +0500

    JS Assignment

---
+++ app.js
@@ -0,0 +1,145 @@
+ // alert("hassan")
+
+  //Chapter no. 2 Q1
+  // var username;
+
+ //Chapter no. 2 Q2
+ //var myName=("muhammad hassan khan");
+ //alert(myName)
+
+ // Chapter no. 2 Q3
+ //var message = "Hello HASSAN";
+ //window.alert(message);
+
+ // Chapter no. 2 Q4
+
+ // var studentName = "My name is HASSAN ARIF";
+ // var studentAge = "I'm 23 years old";
+ // var studentMessage = "I'm a certified MARKETIER";
+
+// window.alert(studentName);
+// window.alert(studentAge);
+// window.alert(studentMessage);
+
+// Chapter no. 2 Q5
+
+// var lineBreak = "HASSAN ARIF\nHASSAN\nARIF"
+// window.alert(lineBreak);
+
+// Chapter no. 2 Q6
+
+// var email = "ha8819598@gmail.com"
+// window.alert(email);
+
+// Chapter no. 2 Q7
+
+// var book = "A smarter way to learn JavaScript";
+// window.alert(book);
+
+// Chapter no. 2 Q8
+
+// document.write("Yah! I can write HTML content through JS");
+
+// Chapter no. 2 Q9
+
+// window.alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+// Chapter no. 3 Q1
+
+ //var age = 23;
+ //window.alert("i am " +age +  "years old");
+
+// Chapter no. 3 Q2
+
+// var n = 10;
+// window.alert("You have visited this site " + n + " Times");
+
+// Chapter no. 3 Q3
+
+// var birthYear = 1999;
+// document.write("My birth year is " + birthYear);
+
+// Chapter no. 3 Q4
+
+ //var vistorName = "Arsalan";
+ //var product = "T-shirt(s)";
+ //var quantity = 1;
+
+ //document.write(vistorName + " ordered " + quantity + " " + product + " on SMARTER clothing store");
+
+
+ // Chapter no. 4 Q1
+
+// var name, age, rollNo;
+
+
+// Chapter no. 4 Q2
+
+//legal variables name
+//var name, my_Name,whatIsYourName,your_Message_In_Text,$myName;
+
+//illegal variable name
+
+//var 1name, my name, var , one*two , function;
+
+// Chapter no. 4 Q3
+
+// var heading = "Rules for naming JS variables"
+// var variableName1 = "$", variableName2 = "_" , variableName3 = "numbers"; 
+// var variableNameBegins1 = "small letter", variableNameBegins2 = "$", variableNameBegins3 = "_";
+// var jsIsCase = "sensitive";
+// var keywords = "keywords";
+
+// document.write(heading + "<br> <br> <br>");
+// document.write("Variable names can only contains " + variableName1 + " , " + variableName2 + " and " + variableName3 + " For Example: $my_1stVariable" + "<br>")
+// document.write("Variable must begins with a " + variableNameBegins1 + " , " + variableNameBegins2 + " & " + variableNameBegins3 + ". For Example: $name, _name, or name" + "<br>");
+// document.write("Variabe names are case " + jsIsCase + "<br>");
+// document.write("Variable names should not be JS " + keywords);
+
+
+// Chapter no.5 Q1
+
+// var num1 = 2, num2 = 4;
+//var add = num1 + num2 ;
+
+// document.write("Sum of " + num1 + " and " + num2 + " is " + add);
+
+// Chapter no.5 Q2 
+
+// var num1 = 20, num2 = 10;
+// var subtract = num1 - num2;
+// var multiplication = num1*num2;
+// var division = num1/num2;
+// document.write(`Subtraction of ${num1} and ${num2} is ${subtract}`);
+// document.write(`Multipication of ${num1} and ${num2} is ${multiplication}`);
+// document.write(`division of ${num1} and ${num2} is ${division}`);
+
+// Chapter no.5 Q3
+
+// var number;
+// document.write("Value after variable declaration is: ??." + "<br>");
+// number = 5;
+// document.write(`Initialize value: ${number} <br>`);
+// number = ++number;
+// document.write(`Value after Increment: ${number} <br>`);
+// number = number + 7;
+// document.write(`Value after addition is: ${number} <br>`);
+// number = --number;
+// document.write(`Value after decrement is: ${number} <br>`);
+// number = number % 3;
+// document.write(`The reminder is: ${number}`);
+
+// Chapter no.5 Q4
+
+// var ticketPrice = 600;
+// document.write(`The total cost to buy 5 tickets to a movie is ${ticketPrice*5}PKR`);
+
+// Chapter no.5 Q5
+
+// var number = 4;
+
+// document.write("Table of 4" + "<br> <br>");
+// for(var i=1 ; i<=10 ; i++){
+//     document.write(`${number}x${i}=${number*i} <br>`);
+// }
+
